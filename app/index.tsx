691f8a68b2888169f486abb19ae0816a21217eb6
Rohan Bhatia <mr.bhatia07@gmail.com>
2020-05-17 19:40:01 +0530

    Added react-navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,16 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import {NavigationContainer} from '@react-navigation/native';
+
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </NavigationContainer>
   );
 };
 
