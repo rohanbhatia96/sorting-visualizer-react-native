ceef7292bea6fb5a3865866d5824b8b6a3b14d0b
Rohan Bhatia <mr.bhatia07@gmail.com>
2020-05-18 00:20:01 +0530

    Modified the initial screen

--- app/screens/InitialScreen/Initial.tsx
+++ app/screens/InitialScreen/Initial.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Button} from 'react-native-elements';
+import styles from './styles';
 
-const Initial = () => {
+const Initial: React.FC = () => {
+  const buttons: string[] = ['Merge Sort', 'Quick Sort'];
+  const ButtonGroup: React.FC = () => {
+    const buttonGroup = buttons.map((button: string) => (
+      <Button key={button} title={button} buttonStyle={styles.button} />
+    ));
+    return <>{buttonGroup}</>;
+  };
+
   return (
-    <View>
-      <Text>Hello Initial Screen</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.headingContainer}>
+        <Text style={styles.appHeading}>Sorting Visualizer</Text>
+        <Text style={styles.authorHeading}>by Rohan Bhatia</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <Text style={styles.questionText}>
+          Which sort would you like to visualize?
+        </Text>
+        <ButtonGroup />
+      </View>
     </View>
   );
 };
--- app/screens/InitialScreen/styles.ts
+++ app/screens/InitialScreen/styles.ts
@@ -1,12 +1,45 @@
-import {StyleSheet, ViewStyle} from 'react-native';
+import {StyleSheet, ViewStyle, TextStyle} from 'react-native';
 
 interface Styles {
   mainContainer: ViewStyle;
+  headingContainer: ViewStyle;
+  contentContainer: ViewStyle;
+  appHeading: TextStyle;
+  authorHeading: TextStyle;
+  questionText: TextStyle;
+  button: ViewStyle;
 }
 
 const styles = StyleSheet.create<Styles>({
   mainContainer: {
     flex: 1,
+  },
+  headingContainer: {
+    flex: 0.35,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contentContainer: {
+    flex: 0.65,
+    alignItems: 'center',
+    paddingHorizontal: 40,
+  },
+  appHeading: {
+    fontSize: 32,
+    color: 'gray',
+  },
+  authorHeading: {
+    color: 'gray',
+  },
+  questionText: {
+    paddingBottom: 30,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  button: {
+    width: 200,
+    paddingVertical: 15,
+    marginVertical: 5,
   },
 });
 
